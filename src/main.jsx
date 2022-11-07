e67b5ffd2bb30cfb2b73964b3647238fae32c1d7
Luke Sheppard <sheppiegit@gmail.com>
2022-11-07 14:30:40 -0500

    Added a data loader to grab fake data from the contact.js file and pass it into the Root component with the useLoaderData.  As well, this had to be passed as a loader in router on the main.jsx file.

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ErrorPage } from "./error-page";
 
 import "./index.css";
-import { Root } from "./routes/root";
+import { Contact } from "./routes/contact";
+import { Root, loader as rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
